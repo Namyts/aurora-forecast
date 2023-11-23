e9f5817a4c04d8d1104bf4c6f1d965578f25926c
Namyts <35004248+Namyts@users.noreply.github.com>
2023-11-23 17:14:09 +0000

    added logic to detect the latest icelandic met office report within the last 20 hours

--- src/App.js
+++ src/App.js
@@ -1,114 +1,149 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useMemo, useState} from 'react'
 import classes from './App.module.css'
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 import './sliderOverrides.css'
 
-const sliderMin = 1
-const sliderMax = 66
 
-const getStartDate = () => {
-	let date = new Date()
-	const currentHours = date.getHours()
-	const detectionHours = [18,12,6,0]
-	const websiteUpdateTime = 2
-	const detectionHour = detectionHours.find(dh=>currentHours-websiteUpdateTime>=dh)
-	date.setHours(detectionHour)
-	date.setMinutes(0)
-	date.setSeconds(0)
-	date.setMilliseconds(0)
-	return date
+const forecastOptions = [
+	{id: 'lowmid', text: 'Low/Mid', url: 'isl_skyjahula2'},
+	{id: 'low', text: 'Low', url: 'harmonie_island_lcc'},
+	{id: 'mid', text: 'Mid', url: 'harmonie_island_mcc'},
+	{id: 'high', text: 'High', url: 'harmonie_island_hcc'}
+]
+
+const addHours = (d,forecastSlider=1) => d && new Date(new Date(d).setHours(d.getHours() + forecastSlider))
+const getDayName = d => {
+	const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+	return d && weekday[d.getDay()]
 }
 
-const getCurrentDate = (forecastSlider=1) => {
-	const d = getStartDate()
-	d.setHours(d.getHours() + forecastSlider)
-	return d
-}
+const loadImage = url => new Promise((resolve,reject)=>{
+	const img = new Image()
+	img.onload = resolve
+	img.onerror = reject
+	img.src = url
+})
 
-
-const forecastOptions = {
-	lowmid: 'Low/Mid',
-	low: 'Low',
-	mid: 'Mid',
-	high: 'High'
-}
-
-const getForecastImage = (slider, type) => {
+const getForecastImage = (d, slider=1, type='lowmid', caller) => {
+	if(!d){return ''}
 	let url = `https://en.vedur.is/photos/`
-	switch(type){
-		case 'lowmid': url+="isl_skyjahula2"; break
-		case 'low': url+="harmonie_island_lcc"; break
-		case 'mid': url+="harmonie_island_mcc"; break
-		case 'high': url+="harmonie_island_hcc"; break
-		default: url+="isl_skyjahula2"; break
-	}
 
-	const d = getStartDate()
-
+	url += forecastOptions.find(fo=>fo.id===type)?.url
 	url += `/${d.getFullYear().toString().slice(2)}${d.getMonth()+1}${d.getDate()}_${d.getHours().toString().padStart(2,'0')}00_${slider}.png`
 
-	// console.log(d)
-	// console.log(url)
-	// console.log("https://en.vedur.is/photos/isl_skyjahula2/231121_1200_1.png")
 	return url
 }
 
-const getDayName = d => {
-	const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-	return weekday[d.getDay()]
-}
-
-const formatDate = d => `${getDayName(d)} ${d.getHours()}:00`
-
-const getMarks = () => {
-	let marks = []
-	for(let i=sliderMin; i<sliderMax; i++){
-		if(getCurrentDate(i).getHours()===0){
-			marks.push([i,getDayName(getCurrentDate(i))])
-		}
-	}
-	const style = {color: "white"}
-	return Object.fromEntries(marks.map(m=>[m[0],{style, label:m[1]}]))
-}
-
-const preloadImages = () => {
-	Object.keys(forecastOptions).forEach(fo=>{
-		for(let i=sliderMin; i<sliderMax; i++){
-			const img = new Image()
-			img.src = getForecastImage(i,fo)
+const preloadImages = (d, min, max) => {
+	forecastOptions.forEach(fo=>{
+		for(let i=min; i<max; i++){
+			loadImage(getForecastImage(d,i,fo.id,'pi'))
 		}
 	})
 }
 
+const determineForecastStart = () => {
+	let attempts = 20
+	const checkDate = d => (
+		loadImage(getForecastImage(d,1,undefined,'determine'))
+		.then(()=>{
+			console.log(`Start date is: ${d.toString()}`)
+			return d
+		})
+		.catch(()=>{
+			attempts = attempts - 1
+			if(attempts>0){
+				return Promise.resolve(checkDate(addHours(d,-1)))
+			} else {
+				return Promise.reject('Couldnt find working image!')
+			}
+		})
+	)
+
+	let date = new Date()
+	date.setMinutes(0,0,0)
+	return checkDate(date)
+}
+
 const App = () => {
 
-	const cycleForecastType = () => {
-		const fi  = Object.keys(forecastOptions).indexOf(forecastType)
-		const nfi = (fi + 1) % Object.keys(forecastOptions).length
-		setForecastType(Object.keys(forecastOptions)[nfi])
-	}
+	const sliderMin = 1
+	const sliderMax = 66
 
+	const [forecastStart, setForecastStart] = useState()
+	const [status, setStatus] = useState('loading')
 	const [forecastSlider, setForecastSlider] = useState(1)
 	const [forecastType, setForecastType] = useState('lowmid')
 
-	useEffect(()=>preloadImages(),[])
+	useEffect(()=>forecastStart && preloadImages(forecastStart,sliderMin,sliderMax),[forecastStart])
+	useEffect(()=>{
+		if(!forecastStart){
+			determineForecastStart()
+			.then(fs=>{
+				setForecastStart(fs)
+				setStatus('ok')
+			})
+			.catch(()=>setStatus('error'))
+		}
+	},[])
+
+	const cycleForecastType = () => {
+		const types = forecastOptions.map(fo=>fo.id)
+		const fi  = types.indexOf(forecastType)
+		const nfi = (fi + 1) % types.length
+		setForecastType(types[nfi])
+	}
+
+	const formatDateText = d => d && `${getDayName(d)} ${d.getHours()}:00`
+
+	const marks = useMemo(() => {
+		if(!forecastStart){return {}}
+		let marks = {}
+		for(let i=sliderMin; i<sliderMax; i++){
+			const checkedDate = addHours(forecastStart,i)
+			if(checkedDate.getHours()===0){
+				marks[i] = {
+					label: getDayName(checkedDate),
+					style: {color: "white"}
+				}
+			}
+		}
+		return marks
+	},[forecastStart,sliderMin,sliderMax])
 
 	return (
 		<div className={classes['container']}>
 			<div className={classes['header']}>
 				<div className={classes['menu']} onClick={cycleForecastType}>
-					<div className={classes['text']}>🔁{forecastOptions[forecastType]}</div>
+					<div className={classes['text']}>
+						🔁{forecastOptions.find(fo=>fo.id===forecastType)?.text}
+					</div>
 				</div>
 				<div className={classes['date']}>
-				<div className={classes['text']}>{formatDate(getCurrentDate(forecastSlider))}</div>
+					<div className={classes['text']}>
+						{formatDateText(addHours(forecastStart,forecastSlider))}
+					</div>
 				</div>
 			</div>
-			<div className={classes['image-container']} >
-				<img className={classes['image']} src={getForecastImage(forecastSlider, forecastType)}/>
+			<div className={classes['image-container']}>
+				{status === 'loading' && <div className={classes['text']}>Loading...</div>}
+				{status === 'error' && <div className={classes['text']}>Error!</div>}
+				{status === 'ok' && (
+					<img
+						className={classes['image']}
+						src={getForecastImage(forecastStart,forecastSlider,forecastType,'img')}
+					/>
+				)}
 			</div>
 			<div className={classes['slider-container']}>
-				<Slider className={classes['slider']} min={sliderMin} max={sliderMax} onChange={setForecastSlider} marks={getMarks()}/>
+				<Slider
+					className={classes['slider']}
+					min={sliderMin}
+					max={sliderMax}
+					onChange={setForecastSlider}
+					marks={marks}
+				/>
 			</div>
 		</div>
 	)
